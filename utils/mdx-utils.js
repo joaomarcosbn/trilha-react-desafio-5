1ae79f2e2769b0d57d4822b4d6156aa4a0ac9ab3
João Marcos Batista Nunes <joaomarcosn@ymail.com>
2026-03-02 11:33:31 -0300

    commit: create getPostBySlug function

--- utils/mdx-utils.js
+++ utils/mdx-utils.js
@@ -1,19 +1,31 @@
-import { api } from '../services/api'
+import { api } from '../services/api';
 
 export const getPosts = async () => {
-    const {data} = await api.get('/posts'); 
+  try {
+    const { data } = await api.get('/posts');
 
-    if(data){
-        return data;
+    if (data) {
+      return data;
     }
 
-    return []
-}
+    return [];
+  } catch (error) {
+    console.error('Erro ao buscar a lista de posts:', error);
+    return [];
+  }
+};
 
 export const getPostBySlug = async (id) => {
+  try {
+    const { data } = await api.get(`/posts?id=eq.${id}`);
 
-    //TODO: BUSCAR UM POST EM ESPECIFICO.
-    //const {data} = await api.get(`/post?id=eq.${id}`)
+    if (data && data.length > 0) {
+      return data[0];
+    }
 
-    return {}
-}
+    return {};
+  } catch (error) {
+    console.error('Erro ao buscar o post específico:', error);
+    return {};
+  }
+};
